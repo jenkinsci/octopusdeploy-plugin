9ecb8eec58fbf465a7a5c7c936948ae2e93e7fa4
Brian Adriance <badriance@vistaprint.com>
2015-05-27 08:33:11 -0400

    Adding form validation for octopus URL

--- src/main/java/hudson/plugins/octopusdeploy/OctopusDeployPlugin.java
+++ src/main/java/hudson/plugins/octopusdeploy/OctopusDeployPlugin.java
@@ -5,6 +5,11 @@ import org.kohsuke.stapler.*;
 import net.sf.json.JSONObject;
 import hudson.Extension;
 import hudson.model.Descriptor;
+import hudson.util.FormValidation;
+import java.io.IOException;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This plugin is only responsible for containing global configuration information
@@ -32,12 +37,44 @@ public class OctopusDeployPlugin extends GlobalPluginConfiguration {
             return "OctopusDeploy Plugin";
         }
 
+        public FormValidation doCheckOctopusHost(@QueryParameter String octopusHost) {
+            if ("".equals(octopusHost)) {
+                return FormValidation.warning("Please enter a url to your OctopusDeploy Host");
+            }
+            URL url;
+            try {
+                url = new URL(octopusHost);
+            } catch (MalformedURLException ex) {
+                final String INVALID_URL = "Supplied Octopus Host URL is invalid";
+                return FormValidation.error(INVALID_URL);
+            }
+            
+            try {
+                URLConnection connection = url.openConnection();
+                if (connection instanceof HttpURLConnection) {
+                    HttpURLConnection httpConnection = (HttpURLConnection)connection;
+                    httpConnection.setRequestMethod("HEAD");
+                    int code = httpConnection.getResponseCode();
+                    httpConnection.disconnect();
+                    if (code >= 400)
+                    {
+                        return FormValidation.error("Could not connect. HTTP Response %s", code);
+                    }
+                }
+            } catch (IOException ex) {
+                final String UNABLE_TO_CONNECT = "Unable to connect to Octopus Host URL";
+                return FormValidation.error("%s - %s", UNABLE_TO_CONNECT, ex.getMessage());
+            }
+            
+            return FormValidation.ok();
+        }
+        
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws Descriptor.FormException {
             JSONObject json = formData.getJSONObject("octopusConfig");
             apiKey = json.getString("apiKey");
             octopusHost = json.getString("octopusHost");
-
+            
             save();
             return super.configure(req, formData);
         }
