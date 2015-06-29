c354d1c0474f4b373a9223177ccfe193bb6f44b4
Brian Adriance <badriance@vistaprint.com>
2015-06-29 11:16:06 -0400

    Javadocs updates

--- src/main/java/hudson/plugins/octopusdeploy/BuildInfoSummary.java
+++ src/main/java/hudson/plugins/octopusdeploy/BuildInfoSummary.java
@@ -3,8 +3,7 @@ package hudson.plugins.octopusdeploy;
 import hudson.model.BuildBadgeAction;
 
 /**
- *
- * @author badriance
+ * Plugin portion to show information on the Build Summary page, and on the line entries for build history.
  */
 public class BuildInfoSummary implements BuildBadgeAction {
     
@@ -15,13 +14,21 @@ public class BuildInfoSummary implements BuildBadgeAction {
         this.buildResultType = buildResultType;
         this.urlName = urlName;
     }
-
    
+    /**
+     * The default file for the image used by this summary entry.
+     * @return relative path to a file.
+     */
     @Override
     public String getIconFileName() {
         return "/plugin/octopusdeploy/images/octopus-o.png";
     }
     
+    /**
+     * Get an icon that is differentiated depending on which kind of
+     * action this is representing.
+     * @return icon file path.
+     */
     public String getLabelledIconFileName() {
         String filename;
         switch (buildResultType) {
@@ -38,16 +45,27 @@ public class BuildInfoSummary implements BuildBadgeAction {
         return "/plugin/octopusdeploy/images/" + filename;
     }
 
+    /**
+     * Display name for this summary entry.
+     * @return OctopusDeploy - [the type of action this represents]
+     */
     @Override
     public String getDisplayName() {
         return "OctopusDeploy - " + buildResultType;
     }
 
+    /**
+     * The URL to use in this summary entry.
+     * @return URL to link to.
+     */
     @Override
     public String getUrlName() {
         return urlName;
     }
     
+    /**
+     * The types of OctopusDeploy even that this class can represent.
+     */
     public enum OctopusDeployEventType {
         Deployment, Release
     }
