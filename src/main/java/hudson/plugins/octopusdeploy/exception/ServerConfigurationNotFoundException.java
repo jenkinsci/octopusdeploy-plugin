1baaa9029326003bc02e00da6f04fd528437e4b2
Ben Pearce <ben.pearce@octopus.com>
2020-09-02 14:32:49 +1000

    Added helper text to the server not found exception

--- src/main/java/hudson/plugins/octopusdeploy/exception/ServerConfigurationNotFoundException.java
+++ src/main/java/hudson/plugins/octopusdeploy/exception/ServerConfigurationNotFoundException.java
@@ -2,6 +2,10 @@ package hudson.plugins.octopusdeploy.exception;
 
 public class ServerConfigurationNotFoundException extends Exception {
     public ServerConfigurationNotFoundException(String serverId) {
-        super(serverId == null || serverId.isEmpty() ? "Server Id not supplied and no default server could be found" : "Server configuration, " + serverId + ", not found.");
+        super(
+                serverId == null || serverId.isEmpty()
+                        ? "Server Id not supplied and no default server could be found"
+                        : "Server configuration, '" + serverId + "', not found. The serverId parameter needs to match the Server ID of a configured OctopusDeploy server at Manage Jenkins -> Configure System -> OctopusDeploy Plugin");
     }
 }
+
